61320cd596b3e3b07cac36948028b667a1cd5de6
Deivison <deivisonlb@gmail.com>
2011-02-12 15:14:59 -0200

    Criacao do metodo locarFilme na classe Socio

--- test/SocioTest.java
+++ test/SocioTest.java
@@ -15,6 +15,6 @@ import locadora.Socio;
 public class SocioTest extends TestCase {
      public void testLocarFilme(int codigo, int inscricao, Date data_emprestimo){
          Socio socio = new Socio();
-         socio.locarFilme();
+         socio.locarFilme(codigo, inscricao, data_emprestimo);
 }
 }
