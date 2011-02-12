a444cef4b0b9909eec72fb83f1b89b3113b5e1e3
Deivison <deivisonlb@gmail.com>
2011-02-12 14:08:06 -0200

    remocao de linhas na classe

--- test/SocioTest.java
+++ test/SocioTest.java
@@ -13,10 +13,6 @@ import junit.framework.*;
  */
 public class SocioTest extends TestCase {
      public void testLocarFilme(int codigo, int inscricao, Date data_emprestimo){
-         System.out.print("testei");
-         System.out.print("testei de novo");
-         System.out.print("blablabla");
-    System.out.print("teste");
     
      
 }
