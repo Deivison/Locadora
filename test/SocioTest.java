ed14673bde7550ade926cb88fc7cb7fb61184c49
Deivison <deivisonlb@gmail.com>
2011-02-12 14:25:19 -0200

    inclusao duas linhas na classe

--- test/SocioTest.java
+++ test/SocioTest.java
@@ -14,6 +14,7 @@ import junit.framework.*;
 public class SocioTest extends TestCase {
      public void testLocarFilme(int codigo, int inscricao, Date data_emprestimo){
          System.out.print("1 linha");
+         System.out.print("1 linha");
     
      
 }
