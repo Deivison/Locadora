7dfb05891854e72f9be6c61e75f61749ed956f63
Deivison <deivisonlb@gmail.com>
2011-02-12 13:54:32 -0200

    mensagem

---
+++ test/SocioTest.java
@@ -0,0 +1,20 @@
+
+import java.util.Date;
+import junit.framework.*;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Deivison
+ */
+public class SocioTest extends TestCase {
+     public void testLocarFilme(int codigo, int inscricao, Date data_emprestimo){
+         System.out.print("testei");
+         System.out.print("testei de novo");
+     
+}
+}
