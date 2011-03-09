e0a13bb41760ccab595dcaf3b35cec897c8f3ae3
Deivison <deivisonlb@gmail.com>
2011-03-09 10:45:55 -0300

    Atualizacoes

--- test/SocioTest.java
+++ test/SocioTest.java
@@ -1,6 +1,6 @@
 
-import java.util.Date;
-import junit.framework.*;
+import junit.framework.Assert;
+import junit.framework.TestCase;
 import locadora.Socio;
 
 /*
@@ -13,8 +13,42 @@ import locadora.Socio;
  * @author Deivison
  */
 public class SocioTest extends TestCase {
-     public void testLocarFilme(int codigo, int inscricao, Date data_emprestimo){
-         Socio socio = new Socio();
-         socio.locarFilme(codigo, inscricao, data_emprestimo);
-}
+    
+    Socio[] socio = new Socio[10];
+    private int inteiroTestado;
+    private String stringTestada;
+    private int posicao = 0;
+
+    public void inicializaSocio(){
+        socio[posicao] = new Socio();
+    }
+    
+    public void testInscricao(){
+        inicializaSocio();
+        inteiroTestado = 10;
+        socio[posicao].setInscricao(inteiroTestado);
+        assertEquals(inteiroTestado, socio[posicao].getInscricao());
+    }
+
+    public void testNome(){
+        inicializaSocio();
+        stringTestada = "João";
+        socio[posicao].setNome(stringTestada);
+        assertEquals(stringTestada, socio[posicao].getNome());
+    }
+
+    public void testEndereco(){
+        inicializaSocio();
+        stringTestada = "Rua Projetada, S/N - Centro - Campos dos Goytacazes - RJ";
+        socio[posicao].setEndereco(stringTestada);
+        assertEquals(stringTestada, socio[posicao].getEndereco());
+    }
+
+    public void testTelefone(){
+        inicializaSocio();
+        stringTestada = "2722-2222";
+        socio[posicao].setTelefone(stringTestada);
+        assertEquals(stringTestada, socio[posicao].getTelefone());
+    }
+
 }
